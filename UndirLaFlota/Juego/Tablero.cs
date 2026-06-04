25ca910bc8e006ea52110cef059e34eafffcca4f
Sara Yue Madruga Martín <smm1032@alu.ubu.es>
2026-06-04 12:56:55 +0200

    Tablero de Jugador 2
    - Se ha implementado otro tablero para un segundo jugador.
    - El tablero del jugador 1 no se le añaden barcos para que pueda luego colocarlos él manualmente.

--- UndirLaFlota/Juego/Tablero.cs
+++ UndirLaFlota/Juego/Tablero.cs
@@ -1,5 +1,3 @@
-
-
 namespace UndirLaFlota.Juego;
 
 public class Tablero
@@ -9,9 +7,12 @@ public class Tablero
     private int aciertos = 0;
     private int TotalPuntos;
     public int Dim;
-    
 
-    public Tablero()
+    /// <summary>
+    /// Método que gestiona la lógica para añadir los barcos en el tablero.
+    /// </summary>
+    /// <param name="iaBoard">Booleano para indicar si se deben generan o no barcos en el tablero creado.</param>
+    public Tablero(bool iaBoard)
     {
         Dim = 10;
         TableroList = new List<List<int>>();
@@ -21,26 +22,33 @@ public class Tablero
         Barcos.Add(3);
         Barcos.Add(3);
         Barcos.Add(2);
+
         foreach (int i in Barcos)
-        {
             TotalPuntos += i;
-        }
-        
+
         TableroLimpio();
-        GenerarTablero();
+
+        if(iaBoard)
+            GenerarTablero();
     }
 
+    /// <summary>
+    /// Método para generar el tablero con los barcos.
+    /// </summary>
     public void GenerarTablero()
     {
         foreach (int i in Barcos)
-        {
             GenerarBarco(i);
-        }
     }
 
+    /// <summary>
+    /// Método que gestiona la lógica de implementar los barcos teniendo en cuenta su tamaño y el tablero.
+    /// </summary>
+    /// <param name="tamano"></param>
     private void GenerarBarco(int tamano)
     {
         Random random = new Random();
+
         int x=0; 
         int y=0; 
         int dir=0;
@@ -48,6 +56,7 @@ public class Tablero
         int P_y=0;
         bool entra = false;
         int pos;
+
         while (!entra)
         {
             entra = true;
@@ -57,6 +66,7 @@ public class Tablero
             P_x = x;
             P_y = y;
             pos = 0;
+
             while (pos<tamano)
             {
                 if (P_x<0 || P_x>=10 || P_y<0 || P_y>=10)
@@ -64,6 +74,7 @@ public class Tablero
                     entra = false;
                     break;
                 }
+
                 else if (TableroList[P_x][P_y] != 0)
                 {
                     entra = false;
@@ -85,11 +96,14 @@ public class Tablero
                         P_y--;
                         break;
                 }
+
                 pos++;
             }
         }
+
         P_x = x;
         P_y = y;
+
         if (entra)
         {
             pos = 0;
@@ -97,6 +111,7 @@ public class Tablero
             while (pos<tamano)
             {
                 TableroList[P_x][P_y] = 2;
+
                 switch (dir)
                 {
                     case 0:
@@ -112,50 +127,56 @@ public class Tablero
                         P_y--;
                         break;
                 }
-                
+
                 pos++;
             }
         }
     }
 
+    /// <summary>
+    /// Método para reiniciar el tablero de nuevo.
+    /// </summary>
     private void TableroLimpio()
     {
         aciertos = 0;
+
         for (int i = 0; i < Dim; i++)
         {
             TableroList.Add(new List<int>());
+
             for (int j = 0; j < Dim; j++)
-            {
                 TableroList[i].Add(0);
-            }
         }
     }
 
+    /// <summary>
+    /// Método que gestiona las jugadas en el tablero.
+    /// Indica si el jugador ha hecho click en una casilla de agua, o ha tocado un barco.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
     public String Jugada(int x, int y)
     {
         if (TableroList[x][y] == 0)
         {
             TableroList[x][y] = 1;
             return "Agua";
-        }else if (TableroList[x][y] == 1 || TableroList[x][y] == 3)
-        {
-            return null;
         }
+        
+        else if (TableroList[x][y] == 1 || TableroList[x][y] == 3) return null;
+
         else if (TableroList[x][y] == 2)
         {
             aciertos ++;
             TableroList[x][y] = 3;
+
             if ( aciertos >= TotalPuntos )
-            {
                 return "Partida finalizada";
-            }
+
             return "Tocado";
         }
 
-       
-
         return null;
     }
-
-
 }
--- UndirLaFlota/MainPage.xaml.cs
+++ UndirLaFlota/MainPage.xaml.cs
@@ -1,71 +1,129 @@
-﻿using UndirLaFlota.Juego;
+﻿using System.Diagnostics;
+using UndirLaFlota.Juego;
 
-namespace UndirLaFlota
+namespace UndirLaFlota;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    // Referencias a la clase "Tablero".
+    private Tablero boardPlayer01; // Tablero del jugador 1.
+    private Tablero boardPlayer02; // Tablero del jugador 2 (IA).
+
+    public MainPage()
     {
-        private Tablero tab;
-        public MainPage()
-        {
-            tab = new Tablero();
-            InitializeComponent();
+        InitializeComponent();
 
-            for (int i = 0; i < tab.Dim; i++)
+        boardPlayer01 = new Tablero(false); // Para el jugador no se le generan barcos, así puede montar él su tablero como quiera.
+        boardPlayer02 = new Tablero(true); // A la IA se le genera el tablero automáticamente.
+
+        InitializeBoard(TableroGridPlayer01, boardPlayer01);
+        InitializeBoard(TableroGridPlayer02, boardPlayer02);
+    }
+
+    /// <summary>
+    /// Método para crear el tablero con las configuraciones pertinentes.
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <param name="board"></param>
+    private void InitializeBoard(Grid grid, Tablero board)
+    {
+        ConfigureGrid(grid, board.Dim);
+        AddHeaders(grid, board.Dim);
+        BuildBoard(board, grid);
+    }
+
+    /// <summary>
+    /// Método para definir las dimensiones del tablero.
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <param name="dim"></param>
+    private void ConfigureGrid(Grid grid, int dim)
+    {
+        grid.RowDefinitions.Clear();
+        grid.ColumnDefinitions.Clear();
+
+        for (int i = 0; i <= dim; i++)
+        {
+            grid.RowDefinitions.Add(new RowDefinition{Height = GridLength.Star});
+            grid.ColumnDefinitions.Add(new ColumnDefinition{Width = GridLength.Star});
+        }
+    }
+
+    /// <summary>
+    /// Método para añadir letras y números para indicar cada columna y fila.
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <param name="dim"></param>
+    private void AddHeaders(Grid grid, int dim)
+    {
+        // Añadir fila indicando con números.
+        for (int i = 1; i <= dim; i++)
+            grid.Add(new Label { Text = i.ToString(), HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center }, i, 0);
+
+        // Añadir columna indicando con letras.
+        for (int i = 1; i <= dim; i++)
+            grid.Add(new Label { Text = ((char)('A' + i - 1)).ToString(), HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center }, 0, i);
+    }
+
+    private void BuildBoard(Tablero board, Grid playerGrid)
+    {
+        for (int row = 0; row < board.Dim; row++)
+        {
+            for (int col = 0; col < board.Dim; col++)
             {
-                for (int j = 0; j < tab.Dim; j++)
+                string txt = board.TableroList[row][col].ToString();
+
+                // Ocultar barcos
+                if (txt == "2")
+                    txt = "0";
+
+                var button = new Button
                 {
-                    String Txt = tab.TableroList[i][j].ToString();
-                    if (Txt.Equals("2"))
-                    {
-                        Txt = "0";
-                    }
-                    var button = new Button
-                    {
-                        Text = Txt,
-                        CommandParameter = new Tuple<int, int>(i, j)
-                    };
+                    Text = txt,
+                    FontSize = 10,
+                    Padding = 0,
+                    Margin = 1,
+                    HorizontalOptions = LayoutOptions.Fill,
+                    VerticalOptions = LayoutOptions.Fill,
+                    CommandParameter = new Tuple<int, int>(row, col)
+                };
 
-                    button.Clicked += (sender, e) =>
-                    {
+                button.Clicked += (sender, e) =>
+                {
+                    var btn = (Button)sender!;
+                    var position = (Tuple<int, int>)btn.CommandParameter;
 
-                        var btn = sender as Button;
-                        var position = (Tuple<int, int>)btn.CommandParameter;
-                        int row = position.Item1;
-                        int column = position.Item2;
+                    Seleccion(board, btn, position.Item1, position.Item2);
+                };
 
-                        // Función que deseas ejecutar
-                        Seleccion(btn, row, column);
-                    };
-
-                    TableroGrid.Add(button, i + 1, j + 1);
-
-
-                }
+                playerGrid.Add(button, col + 1, row + 1);
             }
         }
+    }
 
-        public void Seleccion(Button btn, int row, int column)
+    private async void Seleccion(Tablero board, Button btn, int row, int column)
+    {
+        string? result = board.Jugada(row, column);
+
+        if (result == null)
+            return;
+
+        switch (result)
         {
-            String str = tab.Jugada(row, column);
-            if (str == null)
-            {
-
-            }
-            else if (str.Equals("Agua"))
-            {
+            case "Agua":
                 btn.Text = "1";
-            }
-            else if (str.Equals("Tocado"))
-            {
+                break;
+
+            case "Tocado":
                 btn.Text = "3";
-            }
-            
-            else if (str.Equals("Partida finalizada"))
-            {
-                DisplayAlert("Partidad", "Partida Finalizada!!", "OK");
-            }
+                break;
+
+            case "Partida finalizada":
+                await DisplayAlert(
+                    "Partida",
+                    "¡Partida finalizada!",
+                    "OK");
+                break;
         }
-
-
     }
 }
